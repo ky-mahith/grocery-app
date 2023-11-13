dde34388ff54b89d3fb87e977e33a012cd7c9366
Mahith <mahithkatta@gmail.com>
2023-11-13 17:49:18 -0500

    modified webpage and added scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputText.trim() !== '') {
+      setItems([...items, inputText]);
+      setInputText('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>MY TA TEST</h1>
+        <nav>
+          <a href="https://github.com">GitHub Link to this project</a>
+        </nav>
       </header>
+      <div className="container">
+        <InputComponent
+          inputText={inputText}
+          onInputChange={handleInputChange}
+          onAddItem={handleAddItem}
+        />
+        <ListComponent items={items} />
+      </div>
+    </div>
+  );
+}
+
+function InputComponent({ inputText, onInputChange, onAddItem }) {
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        value={inputText}
+        onChange={onInputChange}
+        placeholder="Enter an item"
+      />
+      <button onClick={onAddItem}>Add</button>
+    </div>
+  );
+}
+
+function ListComponent({ items }) {
+  return (
+    <div className="list-container">
+      <h2>Items List</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
